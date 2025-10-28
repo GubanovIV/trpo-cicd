dd9b27954f7fa89948a99e6dcda44021a44da78d
Gubanov Ivan <ivan_loading@mail.ru>
2025-10-28 12:12:16 +0300

    Add new test

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,7 +1,15 @@
-from app.main import greet
+from app.main import greet, xor
 
 def test_greet_output(capsys):
     greet()
     captured = capsys.readouterr()
     assert captured.out == "Hello world\n"
     assert captured.err == ""
+
+def test_dev_func():
+    ans = {(0, 0): 0,
+           (0, 1): 1,
+           (1, 0): 1,
+           (1, 1): 0,}
+    for k,v in ans.items():
+        assert xor(*k) == v
