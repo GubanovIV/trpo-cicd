bffb012ae9a3c47afe4905176d9d8bf659f5e747
Gubanov Ivan <ivan_loading@mail.ru>
2025-11-10 15:12:45 +0300

    Fixes

--- app/demo_class.py
+++ app/demo_class.py
@@ -2,7 +2,7 @@ class Test:
     """Description of the Test class.
     """
 
-    def a_method(self, a: int, b: int) -> int:
+    def abc_method(self, a: int, b: int) -> int:
         """Description of the function, class or method etc.
 
         Args:
@@ -15,7 +15,7 @@ class Test:
         """
         return a+b
 
-    def b_method(self, a: int, b: int) -> int:
+    def bcd_method(self, a: int, b: int) -> int:
         """Description of the function, class or method etc.
 
         Args:
